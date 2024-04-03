a88c1e1a76c0e08e1a9bb8486bc09aa976813602
juanfer201617 <juanfernandovazquezdiaz@gmail.com>
2024-04-03 16:00:07 -0500

    proyecto terminado

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const URI = `mongodb+srv://JuanVasquez:s1fAN5ZBhIFA3mbE@<nombrecluster>.nzwbkjo.mongodb.net/<nombreBD>?retryWrites=true&w=majority`
+const URI = `mongodb+srv://JuanVasquez:s1fAN5ZBhIFA3mbE@cluster0.q5yk2z0.mongodb.net/cluster0?retryWrites=true&w=majority`
 mongoose.connect(URI);
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -8,7 +8,52 @@ app.use(logger('dev'));
 app.use(exp.urlencoded({extended: false}));
 app.use(exp.json())
 
+app.get('/productos', async (req,res)=>{
+    let listadoProductos = await modeloProducto.find();
+    if(listadoProductos)
+        res.status(200).json(listadoProductos);
+    else
+        res.status(404).json({message: "No se encontraron productos"});    
+});
+
+app.get('/productos/:ref', async (req, res)=>{
+    let productoEncontrado = await modeloProducto.findOne({referencia:req.params.ref});
+    if(productoEncontrado)
+        res.status(200).json(productoEncontrado);
+    else
+        res.status(404).json({"errorr":"Producto no encontrado"})    
+});
+
+
+app.put("/producto/.ref", async (req,res)=>{
+    const productoEditado = {
+        referencia: req.params.ref,
+        nombre: req.body -nombreProducto,
+        descripcion: req.body.descripcionProducto,
+        precio: req.body.precioProducto,
+        stock: req.body.stockProducto,
+        imagen: req.body.imagenProducto,
+        habilitado: true,
+    };
+    let Actualizacion= await modeloProducto.findOneAndUpdate({referencia:req.params.ref},productoEditado);
+    if(Actualizacion)
+        res.status(280).json({"mensaje": "actualización exitoso"})
+    else
+        res.status(404).json({"mensaje": "Se presentó un error"});
+});
+
+app.delete("/producto/.id", async (req,res)=>{
+    console.log(rep.params.id, req.body.referenciaProducto)
+    let eliminacion= await modeloProducto.findOneAndDelete({referencia:req.params.id});
+    if(eliminacion)
+        res.status(200).json({"mensaje":"eliminación exitosa"})
+    else
+        res.status(404).json({"mensaje":"No se presento un error"})
+});
+
+
 app.listen(process.env.PORT, ( )=>{
     console.log("servidor en linea");
-}); 
+});
 
+let modeloProducto = require('./backend/models/productos.model')
